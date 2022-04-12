df906c9d99d378fdab35d6fade905b7e7ff8636e
su <rakdav@inbox.ru>
2022-04-12 15:02:54 +0200

    Добавьте файлы проекта.

---
+++ Stacks/Monitor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks
+{
+    internal class Monitor
+    {
+        public string Name { get; set; }
+        public double Diagonal { get; set; }
+        public decimal Price { get; set; }
+        public Monitor(string name, double diagonal, decimal price)
+        {
+            Name = name;
+            Diagonal = diagonal;
+            Price = price;
+        }
+
+        public override string? ToString()
+        {
+            return $"Название:{Name}, " +
+                $"Диагональ:{Diagonal}, " +
+                $"Цена:{Price}";
+        }
+    }
+}
